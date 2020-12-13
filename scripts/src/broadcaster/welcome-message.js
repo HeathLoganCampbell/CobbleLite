16714026f5ba6df5eb5cd0a9930144f68fe16ec3
Heath L Campbell <34885930+HeathLoganCampbell@users.noreply.github.com>
2020-12-13 16:15:36 +1300

    Update welcome-message.js

--- scripts/src/broadcaster/welcome-message.js
+++ scripts/src/broadcaster/welcome-message.js
@@ -1,4 +1,4 @@
-Events.On('PlayerJoinEvent', ( e ) => {
+Events.On('org.bukkit.event.player.PlayerMoveEvent', ( e ) => {
     var player = e.getPlayer();
     player.sendMessage(CC.BLUE + "Hello world");
 })
